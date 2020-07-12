dfbc9567c7aa38d0e7bdd3cc8fc5557f91614c7f
Giorgio Andronico <bytecodex2@gmail.com>
2020-07-13 00:02:03 +0200

    Updated base visitor implementation of IDLV parser.

--- app/src/main/java/it/unical/mat/parsers/datalog/idlv/IDLVParserBaseVisitorImplementation.java
+++ app/src/main/java/it/unical/mat/parsers/datalog/idlv/IDLVParserBaseVisitorImplementation.java
@@ -1,16 +1,21 @@
 package it.unical.mat.parsers.datalog.idlv;
 
 
+import it.unical.mat.embasp.languages.datalog.MinimalModel;
 import it.unical.mat.parsers.datalog.DatalogDataCollection;
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.atn.PredictionMode;
 
+import java.util.HashSet;
+
 public class IDLVParserBaseVisitorImplementation extends IDLVParserBaseVisitor<Void> {
 
     private final DatalogDataCollection models;
+    private MinimalModel modelCurrentlyBeingVisited;
 
     public IDLVParserBaseVisitorImplementation(final DatalogDataCollection models) {
         this.models = models;
+        this.modelCurrentlyBeingVisited = null;
     }
 
     public static void parse(DatalogDataCollection models, String atomsList, boolean two_stageParsing) {
@@ -43,13 +48,16 @@ public class IDLVParserBaseVisitorImplementation extends IDLVParserBaseVisitor<V
 
     @Override
     public Void visitPredicate_atom(IDLVParser.Predicate_atomContext ctx) {
-        models.storeAtom(ctx.getText());
+        System.out.println("aggiungo " + ctx.getText());
+        modelCurrentlyBeingVisited.getAtomsAsStringList().add(ctx.getText());
         return null;
     }
 
     @Override
     public Void visitMinimal_model(IDLVParser.Minimal_modelContext ctx) {
-        models.addMinimalModel();
+        System.out.println("Creo modello");
+        modelCurrentlyBeingVisited = new MinimalModel(new HashSet<String>());
+        models.addMinimalModel(modelCurrentlyBeingVisited);
         return visitChildren(ctx);
     }
 }
